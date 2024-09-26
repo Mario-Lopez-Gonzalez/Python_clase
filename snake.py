ed99ef2a36f9d89bbc3efa3634cd6019852c6bea
marlopia <marlopiacj@gmail.com>
2024-09-26 20:33:28 +0200

    Snake comentado y modificado, añadido soporte de WASD, aumento de velocidad de juego y spawn de varias frutas a la vez

--- snake.py
+++ snake.py
@@ -2,119 +2,111 @@ import pygame
 import random
 import time
 
-
+# Velocidad inicial de la serpiente
 snake_speed = 15
 
-# Window size
+# Tamaño de la ventana emergente
 window_x = 720
 window_y = 480
 
-# defining colors
+# Colores del juego
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 
-# Initialising pygame
+# Iniciamos pygame
 pygame.init()
 
-# Initialise game window
-pygame.display.set_caption('GeeksforGeeks Snakes')
+# Abrimos ventana
+pygame.display.set_caption('SNAKE')
 game_window = pygame.display.set_mode((window_x, window_y))
 
-# FPS (frames per second) controller
+# Controlador de fotogramas por segundo
 fps = pygame.time.Clock()
 
-# defining snake default position 
+# Spawnpoint de la serpiente y contador de la posición de la cabeza
 snake_position = [100, 50]
 
-# defining first 4 blocks of snake
+# Definimos las coordenadas de los primeros 4 segmentos de la serpiente
 # body
-snake_body = [  [100, 50],
+snake_body = [  [100, 50], #Coincide con snake_position y nos va a marcar el punto central del personaje
                 [90, 50],
                 [80, 50],
                 [70, 50]
             ]
-# fruit position 
-fruit_position = [random.randrange(1, (window_x//10)) * 10,
-                  random.randrange(1, (window_y//10)) * 10]
+# Cálculo de la posición de la fruta y si aparece en pantalla
+fruit_position = [[random.randrange(1, (window_x//10)) * 10,
+                  random.randrange(1, (window_y//10)) * 10]]
 fruit_spawn = True
 
-# setting default snake direction 
-# towards right
+fruit_uneaten = 0 # Creamos una variable que cree más frutas si el jugador no ha comido una fruta en un tiempo
+
+fruit_eaten = False # Control para saber si nos hemos comido una fruta este frame
+
+# Creamos la dirección de la serpiente y le damos una de comienzo
 direction = 'RIGHT'
 change_to = direction
 
-# initial score
+# Puntos iniciales
 score = 0
 
-# displaying Score function
+# Función para enseñar los puntos
 def show_score(choice, color, font, size):
   
-    # creating font object score_font 
+    # Creamos objeto fuente del puntuaje
     score_font = pygame.font.SysFont(font, size)
     
-    # create the display surface object
-    # score_surface
-    score_surface = score_font.render('Score : ' + str(score), True, color)
+    # Creamos el fondo del puntuaje con render
+    score_surface = score_font.render('Puntos : ' + str(score), True, color)
     
-    # create a rectangular object for the 
-    # text surface object
+    # Creamos un rectangulo para el puntuaje
     score_rect = score_surface.get_rect()
     
-    # displaying text
+    # Sacamos el texto en la ventana superponiendo el fondo creado en el rectangulo designado
     game_window.blit(score_surface, score_rect)
 
-# game over function
+# Definimos el game over
 def game_over():
   
-    # creating font object my_font
+    # Creamos el objeto fuente
     my_font = pygame.font.SysFont('times new roman', 50)
     
-    # creating a text surface on which text 
-    # will be drawn
-    game_over_surface = my_font.render('Your Score is : ' + str(score), True, red)
+    # Creamos el fondo del texto
+    game_over_surface = my_font.render('GAME OVER: ' + str(score), True, red)
     
-    # create a rectangular object for the text
-    # surface object
+    # Creamos el rectangulo del fondo
     game_over_rect = game_over_surface.get_rect()
     
-    # setting position of the text
+    # Coordenadas del texto (en el medio)
     game_over_rect.midtop = (window_x/2, window_y/4)
     
-    # blit will draw the text on screen
+    # Escribimos el texto con blit
     game_window.blit(game_over_surface, game_over_rect)
     pygame.display.flip()
     
-    # after 2 seconds we will quit the 
-    # program
+    # Damos 2s y luego cerramos el programa
     time.sleep(2)
-    
-    # deactivating pygame library
     pygame.quit()
-    
-    # quit the program
     quit()
 
-# Main Function
+# Funcion principal
 while True:
   
-    # handling key events
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                change_to = 'UP'
-            if event.key == pygame.K_DOWN:
+    # Manejo de teclas
+    for event in pygame.event.get(): # Le pedimos a pygame que nos devuelva los eventos
+        if event.type == pygame.KEYDOWN: # Nos enfocamos en las teclas pulsadas
+            if event.key == pygame.K_UP or event.key == ord('w'): # Verificamos que tecla está siendo pulsada
+                change_to = 'UP' # Cambiamos la dirección de la serpiente a la pulsada en el siguiente frame
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
                 change_to = 'DOWN'
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT or event.key == ord('a'): # Implementamos control por WASD
                 change_to = 'LEFT'
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 change_to = 'RIGHT'
 
-    # If two keys pressed simultaneously 
-    # we don't want snake to move into two directions
-    # simultaneously
+    # Bloqueamos movimientos contrarios de teclas priorizando la primera
     if change_to == 'UP' and direction != 'DOWN':
         direction = 'UP'
     if change_to == 'DOWN' and direction != 'UP':
@@ -124,7 +116,7 @@ while True:
     if change_to == 'RIGHT' and direction != 'LEFT':
         direction = 'RIGHT'
 
-    # Moving the snake
+    # Modificamos las posiciones según la tecla pulsada
     if direction == 'UP':
         snake_position[1] -= 10
     if direction == 'DOWN':
@@ -134,46 +126,56 @@ while True:
     if direction == 'RIGHT':
         snake_position[0] += 10
 
-    # Snake body growing mechanism 
-    # if fruits and snakes collide then scores will be 
-    # incremented by 10
-    snake_body.insert(0, list(snake_position))
-    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
-        score += 10
-        fruit_spawn = False
-    else:
-        snake_body.pop()
-        
-    if not fruit_spawn:
-        fruit_position = [random.randrange(1, (window_x//10)) * 10, 
-                          random.randrange(1, (window_y//10)) * 10]
-        
-    fruit_spawn = True
-    game_window.fill(black)
-    
-    for pos in snake_body:
-        pygame.draw.rect(game_window, green, pygame.Rect(
-          pos[0], pos[1], 10, 10))
-        
-    pygame.draw.rect(game_window, white, pygame.Rect(
-      fruit_position[0], fruit_position[1], 10, 10))
+    snake_body.insert(0, list(snake_position)) # Creamos nuevo bloque en la misma posición que estamos y lo colocamos como cabeza
+    # Que hacemos cuando la serpiente se come la fruta
+    for fruit in fruit_position:
+        if snake_position[0] == fruit[0] and snake_position[1] == fruit[1]: # Comparamos las coordenadas x e y del jugador y la fruta
+            score += 10 # Sumamos puntos
+            fruit_position.remove(fruit) # Quitamos la fruta de la lista
+            fruit_spawn = False # Avisamos que no hay fruta
+            snake_speed+=1 # Aceleramos un poco el juego por cada fruta consumida
+            fruit_eaten = True
+            break
 
-    # Game Over conditions
-    if snake_position[0] < 0 or snake_position[0] > window_x-10:
+    if not fruit_eaten:
+        snake_body.pop() # Borramos el camino anterior de la serpiente para animar que avanza
+    fruit_eaten = False
+
+    fruit_uneaten += 1 # Sumamos 1 al contador de fruta sin comer
+
+    if not fruit_spawn or fruit_uneaten > 100: # Si avisamos que no hay fruta spawneamos otra
+        fruit_position.append([random.randrange(1, (window_x//10)) * 10, 
+                          random.randrange(1, (window_y//10)) * 10])
+        fruit_uneaten = 0
+        
+    fruit_spawn = True # Reactivamos el flag de la fruta para no crear duplicadas
+    game_window.fill(black) # Llenamos el fondo de la pantalla de negro
+    
+    for pos in snake_body: # Por cada segmento de la serpiente
+        pygame.draw.rect(game_window, green, pygame.Rect(
+          pos[0], pos[1], 10, 10)) # Dibujamos un rectangulo en la ventada de juego, de color verde,
+                                   # en la posición x e y y de 10x10 pixeles
+
+    for pos in fruit_position:    
+        pygame.draw.rect(game_window, white, pygame.Rect( # Dibujamos la fruta con la misma logica
+        pos[0], pos[1], 10, 10))
+
+    # Condiciones del game over
+    if snake_position[0] < 0 or snake_position[0] > window_x-10: # Comprobamos si estamos tocando la pantalla en los ejes x e y
         game_over()
     if snake_position[1] < 0 or snake_position[1] > window_y-10:
         game_over()
     
-    # Touching the snake body
-    for block in snake_body[1:]:
-        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+    # Comprobamos si estamos tocando algun segmento de la serpiente con la cabeza
+    for block in snake_body[1:]: # Empezamos desde el segundo bloque porque el primero es la cabeza, aunque realisticamente es imposible que el primer bloque toque algo que no sea un bloque a partir del 5to
+        if snake_position[0] == block[0] and snake_position[1] == block[1]: # Comparamos coordenadas
             game_over()
     
-    # displaying score continuously
+    # Dibujamos el puntuaje cada frame
     show_score(1, white, 'times new roman', 20)
     
-    # Refresh game screen
+    # Refrescamos la pantalla
     pygame.display.update()
 
-    # Frame Per Second /Refresh Rate
+    # FPS y tasa de refresco, lo cual corresponde con la velocidad del juego
     fps.tick(snake_speed)
